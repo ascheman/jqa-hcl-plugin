6eadcb4a575668e9e8c05ca77c3c449f65a0acc6
Gerd Aschemann <gerd@aschemann.net>
2022-04-23 18:28:03 +0200

    Clean up Tests

--- src/test/java/net/aschemann/jqasssistant/plugin/hcl/impl/scanner/HclScannerPluginTest.java
+++ src/test/java/net/aschemann/jqasssistant/plugin/hcl/impl/scanner/HclScannerPluginTest.java
@@ -1,9 +1,7 @@
 package net.aschemann.jqasssistant.plugin.hcl.impl.scanner;
 
-import com.buschmais.jqassistant.core.scanner.api.DefaultScope;
 import com.buschmais.jqassistant.core.store.api.model.Descriptor;
 import com.buschmais.jqassistant.plugin.common.test.AbstractPluginIT;
-import net.aschemann.jqassistant.plugin.hcl.api.HclScope;
 import net.aschemann.jqassistant.plugin.hcl.api.model.HclConfigurationDescriptor;
 import org.junit.jupiter.api.Test;
 
@@ -14,32 +12,25 @@ import static net.aschemann.jqassistant.plugin.hcl.api.HclScope.HCL;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class HclScannerPluginTest extends AbstractPluginIT {
+class HclScannerPluginTest extends AbstractPluginIT {
 
     public static final String SIMPLE_HCL_TEST = "/hcl/simple";
     public static final String SIMPLE_TF_LB_CONFIGURATION = "/hcl/terraform/simple-lb";
 
-    //    @BeforeEach
-//    public void resetScanner() {
-//        HclScannerPlugin.resetObjectStore();
-//    }
-//
     @Test
-    public void scanSimpleTfLbConfiguration() throws IOException {
+    void scanSimpleTfLbConfiguration() throws IOException {
         File testFile = new File(getClassesDirectory(HclScannerPluginTest.class), SIMPLE_TF_LB_CONFIGURATION);
         HclConfigurationDescriptor hclConfigurationDescriptor = scan(testFile);
         assertEquals(4, hclConfigurationDescriptor.getFiles().size());
         // TODO add more tests on the graph
-//        store.commitTransaction();
         store.rollbackTransaction();
     }
 
     @Test
-    public void scanSimpleHclFile() throws IOException {
+    void scanSimpleHclFile() throws IOException {
         File testFile = new File(getClassesDirectory(HclScannerPluginTest.class), SIMPLE_HCL_TEST);
         HclConfigurationDescriptor hclConfigurationDescriptor = scan(testFile);
         // TODO add more tests on the graph
-//        store.commitTransaction();
         store.rollbackTransaction();
     }
 
